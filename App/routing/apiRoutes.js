1030d8604bd641e751946b0efaefde9b714278f4
irfanp90 <irfanp90@gmail.com>
2019-04-08 23:33:03 -0500

    update server.js and survey.html

--- App/routing/apiRoutes.js
+++ App/routing/apiRoutes.js
@@ -20,14 +20,48 @@ connection.connect(function(err) {
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     // Selects all of the data from the MySQL profiles table
-    connection.query("SELECT * FROM profiles", function(err, response) {
-      if (err) throw err;
-      //a fun trick for converting mysql's returned 'rowPacketData' obj into more usable JSON
-      var data = JSON.stringify(response);
-      data = JSON.parse(data);
-      // loop over your data converting the string of numbers into an array (using split??)
-      friends = data;
-      res.json(friends);
-    });
+    loadProfiles(res);
+  });
+  app.post("/api/friends", function(req, res) {
+    findMatch();
   });
 };
+
+function loadProfiles(response) {
+  connection.query("SELECT * FROM profiles", function(err, result) {
+    if (err) throw err;
+    friends = result;
+    return response.json(friends);
+  });
+}
+function findMatch() {
+  var bestMatch = {
+    name: "",
+    photo: "url",
+    friendDifference: 1000
+  };
+  console.log(req.body);
+
+  var userData = req.body;
+  var userScores = userData.scores;
+  console.log(userScores);
+  var totalDifference = 0;
+
+  for (var i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+    totalDifference = 0;
+    for (var j = 0; j < friends[i].scores[j]; j++) {
+      totalDifference += Math.abs(
+        parseInt(userScores[j]) - parseInt(friends[i].score[j])
+      );
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+    friends.push(userData);
+    res.json(bestMatch);
+    console.log(bestMatch);
+  }
+}
